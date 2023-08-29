f6b2f66773fe38ff26ee19fc59253f2f9c9170b1
Farooq <fmd894717@gmail.com>
2023-08-29 18:56:43 +0530

    commit 29-08-2023

---
+++ src/main/java/cgg/springboot/SpringbootProjApplication.java
@@ -0,0 +1,16 @@
+package cgg.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//equlas to
+//@Configuration , @EnableAutoConfiguration, @ComponentScan
+public class SpringbootProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProjApplication.class, args);
+	}
+
+}
+
